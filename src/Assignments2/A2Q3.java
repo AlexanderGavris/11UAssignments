7965687df5a3b63ac565ae810dd0ef42031e9b7e
gavra1029 <gavra1029@HRH-IC0027334.wrdsb.ca>
2017-09-20 13:19:07 -0400

    Done A2Q1, A2Q2, A2Q3

---
+++ src/Assignments2/A2Q3.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignments2;
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+
+/**
+ *
+ * @author gavra1029
+ */
+public class A2Q3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //make city
+        City q= new City(); 
+        //make robot
+        Robot ob=new Robot(q,31,40,Direction.EAST ); 
+        //find direction of robot and move
+        while(ob.getDirection()!=Direction.NORTH ){
+            ob.turnLeft();   
+        }
+        while(ob.getStreet()!=0 ){
+           ob.move(); 
+        }
+        ob.turnLeft();
+        while(ob.getDirection()!=Direction.WEST ){
+            ob.turnLeft();
+        }
+        while(ob.getStreet()!=0 ){
+            ob.getAvenue();
+        }
+    }
+}
