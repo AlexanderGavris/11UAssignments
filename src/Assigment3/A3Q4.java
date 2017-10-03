451ce4132b108281476c1499c1b03db95f9997c7
gavra1029 <gavra1029@HRH-IC0027334.wrdsb.ca>
2017-10-03 14:08:48 -0400

    Done A3Q1, A3Q2, A3Q3,A3Q4

---
+++ src/Assigment3/A3Q4.java
@@ -0,0 +1,72 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assigment3;
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+import becker.robots.Wall;
+
+/**
+ *
+ * @author gavra1029
+ */
+public class A3Q4 {
+
+    public static void main(String[] args) {
+        //make a city 
+        City kw = new City();
+        //make a robot
+        Robot ob = new Robot(kw, 3, 3, Direction.SOUTH);
+        //make the 4 2 by 2 walls
+        new Wall(kw, 1, 1, Direction.NORTH);
+        new Wall(kw, 1, 1, Direction.WEST);
+        new Wall(kw, 1, 2, Direction.NORTH);
+        new Wall(kw, 1, 2, Direction.EAST);
+        new Wall(kw, 2, 1, Direction.SOUTH);
+        new Wall(kw, 2, 1, Direction.WEST);
+        new Wall(kw, 2, 2, Direction.SOUTH);
+        new Wall(kw, 2, 2, Direction.EAST);
+        new Wall(kw, 1, 4, Direction.NORTH);
+        new Wall(kw, 1, 4, Direction.WEST);
+        new Wall(kw, 1, 5, Direction.NORTH);
+        new Wall(kw, 1, 5, Direction.EAST);
+        new Wall(kw, 2, 4, Direction.SOUTH);
+        new Wall(kw, 2, 4, Direction.WEST);
+        new Wall(kw, 2, 5, Direction.SOUTH);
+        new Wall(kw, 2, 5, Direction.EAST);
+        new Wall(kw, 4, 1, Direction.NORTH);
+        new Wall(kw, 4, 1, Direction.WEST);
+        new Wall(kw, 4, 2, Direction.NORTH);
+        new Wall(kw, 4, 2, Direction.EAST);
+        new Wall(kw, 5, 1, Direction.SOUTH);
+        new Wall(kw, 5, 1, Direction.WEST);
+        new Wall(kw, 5, 2, Direction.SOUTH);
+        new Wall(kw, 5, 2, Direction.EAST);
+        new Wall(kw, 4, 4, Direction.NORTH);
+        new Wall(kw, 4, 4, Direction.WEST);
+        new Wall(kw, 4, 5, Direction.NORTH);
+        new Wall(kw, 4, 5, Direction.EAST);
+        new Wall(kw, 5, 4, Direction.SOUTH);
+        new Wall(kw, 5, 4, Direction.WEST);
+        new Wall(kw, 5, 5, Direction.SOUTH);
+        new Wall(kw, 5, 5, Direction.EAST);
+        //make 
+        for (int counter = 0; counter < 4; counter = counter + 1) {
+            for (int stepCounter = 0; stepCounter < 4; stepCounter = stepCounter + 1) {
+                ob.move();
+                ob.move();
+                ob.move();
+                if (stepCounter < 3) {
+                    ob.turnLeft();
+                }
+
+            }
+
+
+
+        }
+    }
+}
