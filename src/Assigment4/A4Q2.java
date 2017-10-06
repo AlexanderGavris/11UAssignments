e20130d34064a0fe44ac4acef47c94d04df63566
gavra1029 <gavra1029@HRH-IC0027334.wrdsb.ca>
2017-10-06 13:15:51 -0400

    Done A4Q1

---
+++ src/Assigment4/A4Q2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assigment4;
+
+/**
+ *
+ * @author gavra1029
+ */
+public class A4Q2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
