c6f9c646eb7f3e7f13adeeefbf881f790f51a537
gavra1029 <gavra1029@HRH-IC0027334.wrdsb.ca>
2017-10-11 14:23:54 -0400

    Done A4Q1,A4Q2,A4Q3,A4Q4

---
+++ src/Assigment4/A4Q4.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assigment4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author gavra1029
+ */
+public class A4Q4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        //ask the user for all the $ of 
+        System.out.println("How much does the food for prom cost?");
+        double food = input.nextDouble();
+        System.out.println("How much does the DJ cost?");
+        double dj=input.nextDouble();
+        System.out.println("How much does it cost to rent the hall?");
+         double hall=input.nextDouble();
+        System.out.println("How much does decorations cost? ");
+         double decorations=input.nextDouble();
+        System.out.println("How much does it cost for Staff?");
+         double staff=input.nextDouble();
+        System.out.println("How much for miscellaneous costs?");
+         double miscellaneous=input.nextDouble();
+         System.out.println(food+hall+dj+decorations+staff );
+         double total=food+hall+dj+decorations+staff;
+         //divided by the total by the price of the ticket 
+         double tickets=total/35;
+         tickets = Math.ceil(tickets);
+         System.out.println(tickets+"This is how many tickets youu need");
+         
+    }
+}
