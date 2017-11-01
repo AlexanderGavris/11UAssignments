4cf7783930d564b9b30f99a59c417b373d92d620
gavra1029 <gavra1029@HRH-IC0027334.wrdsb.ca>
2017-11-01 13:55:29 -0400

    DONE ubbi Dubbi!!!!!!!!!!!!!!!!!

--- src/Assignment5/A5Q1.java
+++ src/Assignment5/A5Q1.java
@@ -18,60 +18,45 @@ public class A5Q1 {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner input = new Scanner(System.in);
+        // i did this to stop nthe program 
+        //tWord mean translated word
+        while (true) {
+            //ask for a word
+            System.out.println("Enter a word to translate");
+            String tWord = input.nextLine();
+            boolean foundVowel = false;
 
-
-        System.out.println("Enter a word to translate");
-        String tWord = input.nextLine();
-        boolean foundVowel = false;
-
-
-
-
-
-
-        // convert to lowercase
-        tWord = tWord.toLowerCase();
-        
-        if(tWord.equals ("EXIT")){
-            Brake 
-        }
-        
-        
-        String translated = "";
-        for (int i = 0; i < tWord.length(); i++) {
-            // look for a vowel at spot i
-            if (tWord.charAt(i) == 'a' || tWord.charAt(i) == 'e' || tWord.charAt(i) == 'i'
-                    || tWord.charAt(i) == 'o' || tWord.charAt(i) == 'u' && foundVowel==false ){
-                
-                 
-
-
-                // assemble translation
-                translated = translated + "ub" + tWord.charAt(i);
-                foundVowel=true; 
-
-            } else {
-                translated = translated + tWord.charAt(i);
-                
-                while (i >= 1) {
-                if (tWord.charAt(i ) == 'a' || tWord.charAt(i) == 'e' || tWord.charAt(i) == 'i'
-                        || tWord.charAt(i) == 'o' || tWord.charAt(i) == 'u') {
-                
-                
+            if (tWord.equals("EXIT")) {
+                break;
             }
-             
+            // convert to lowercase
+            tWord = tWord.toLowerCase();
+          
+            String translated = "";
+            for (int i = 0; i < tWord.length(); i++) {
+                // look for a vowel at spot i
+                if ((tWord.charAt(i) == 'a' || tWord.charAt(i) == 'e' || tWord.charAt(i) == 'i'
+                        || tWord.charAt(i) == 'o' || tWord.charAt(i) == 'u') && foundVowel == false) {
                     
+                    // assemble translation
+                    translated = translated + "ub" + tWord.charAt(i);
+                    foundVowel = true;
 
-             
-                } 
+                } else {
+                    // this ment for double vowels
+
+                    if ((tWord.charAt(i) == 'a' || tWord.charAt(i) == 'e' || tWord.charAt(i) == 'i'
+                            || tWord.charAt(i) == 'o' || tWord.charAt(i) == 'u') && foundVowel == true) {
+                        translated = translated + tWord.charAt(i);
+
+                    } else {
+                        translated = translated + tWord.charAt(i);
+                        foundVowel = false;
+                    }
+                }
             }
 
+            System.out.println(tWord + " in Ubbi Dubbi is " + translated);
         }
-        
-        
-        
-        
-        System.out.println(tWord + " in Ubbi Dubbi is " + translated);
-
     }
 }
