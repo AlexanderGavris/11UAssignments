e089655953f8546dc61acc8546f8d3aa010ca4a3
gavra1029 <gavra1029@HRH-IC0027334.wrdsb.ca>
2017-11-14 13:19:54 -0500

    DONE ubbi Dubbi!!!!!!!!!!!!!!!!!

---
+++ src/Assigment6/A6Q1.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assigment6;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author gavra1029
+ */
+public class A6Q1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // create scanner for input
+        Scanner input = new Scanner(System.in);
+        //ask how many people are in the class
+        System.out.println("Please enter the number of students:");
+        int studentsInClass = input.nextInt();    
+        //make an aarry to list the marks of all the students 
+        double[] students = new double[studentsInClass];     
+                
+        //loop to get all info
+        for(int i = 0; i < students.length;i++){
+            // ask what are all the marks 
+        System.out.println("Please enter the number of marks for each student:");
+        
+            
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
