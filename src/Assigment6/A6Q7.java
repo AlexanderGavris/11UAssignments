2aabd6cb4f7ce41f74af12935c7fd40d21dc0385
gavra1029 <gavra1029@HRH-IC0027334.wrdsb.ca>
2017-11-27 14:21:08 -0500

    done A6Q6

--- src/Assigment6/A6Q7.java
+++ src/Assigment6/A6Q7.java
@@ -4,10 +4,38 @@
  */
 package Assigment6;
 
+import java.util.Scanner;
+
 /**
  *
  * @author gavra1029
  */
 public class A6Q7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        //ask to enter #'s 
+        System.out.println("How many numbers do you want to enter:");
+        int amountnum = input.nextInt();
+
+        int[] num = new int[amountnum];
+        
+        for (int i = 0; i < 10; i++) {
+            
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
     
 }
