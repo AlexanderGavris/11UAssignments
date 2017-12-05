809767d3f2de4e45ae3be009bcee1df81c213c68
gavra1029 <gavra1029@HRH-IC0027334.wrdsb.ca>
2017-12-05 14:21:59 -0500

    done A6

--- src/Assigment6/A6Q7.java
+++ src/Assigment6/A6Q7.java
@@ -19,26 +19,28 @@ public class A6Q7 {
 
         Scanner input = new Scanner(System.in);
         //make an array that goes up to 1000
-        int[] numbers = new int[1001];
+        int[] num = new int[1001];
 
         //filled the array starting at 2
-        for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = i;
+        System.out.println("The prime numbers are");
+        for (int i = 0; i < num.length; i++) {
+            num[i] = i;
         }
-        for (int x = 2; x < numbers.length; x++) {
-            if (numbers[x] != 0) {
+        for (int x = 2; x < num.length; x++) {
+            if (num[x] != 0) {
 
-                int p = numbers[x];
-                
+                int p = num[x];
+
 
                 //make a for loop to count multi #s
-                for (int i = 2; i * p < numbers.length; i++) {
-                    numbers[i * p] = 0;
+                for (int i = 2; i * p < num.length; i++) {
+                    num[i * p] = 0;
                 }
                 //print out what all the prim numbers
-                
-                System.out.println("The prime numbers are " + numbers[x]);
+
+                System.out.println(num[x]);
             }
+            
         }
 
 
---
+++ src/Assigment7/A7Q1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assigment7;
+
+/**
+ *
+ * @author gavra1029
+ */
+public class A7Q1 {
+
+    public static void main(String[] args) {
+        
+        
+        
+        
+        
+    }
+}
