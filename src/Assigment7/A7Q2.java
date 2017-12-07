2a9150983566e29b0d9f73b2bfb201c5b63e39b2
gavra1029 <gavra1029@HRH-IC0027334.wrdsb.ca>
2017-12-07 14:20:51 -0500

    done A7q1

--- src/Assigment7/A7Q2.java
+++ src/Assigment7/A7Q2.java
@@ -11,13 +11,36 @@ import java.util.Scanner;
  * @author gavra1029
  */
 public class A7Q2 {
-    public void examGrade(double calu){
-        double mark= 
+
+    public void examGrade(double calPercent) {
+        double percent = calPercent;
+        if (percent < 50) {
+            System.out.println(percent + "=F");
+        } else if (50 < percent && percent < 59) {
+            System.out.println(percent + "+D");
+        } else if (60 < percent && percent < 69) {
+            System.out.println(percent + "=c");
+        } else if (70 < percent && percent < 79) {
+            System.out.println(percent + "=B");
+        } else if (80 < percent) {
+            System.out.println(percent + "=A");
+
+        }
     }
+
     public static void main(String[] args) {
+        A7Q2 test = new A7Q2();
+        
         Scanner input = new Scanner(System.in);
         System.out.println("What is the % that you got the exam?");
-        
+
         double percent = input.nextDouble();
+        
+        test.examGrade(percent);
+        System.out.println("You grade in letter"+ percent );
+        
+        
+        
+        
     }
 }
