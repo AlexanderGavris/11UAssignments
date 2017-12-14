fbd7520fa73a52d98e8e591750bbb39564cc9d8b
gavra1029 <gavra1029@HRH-IC0027334.wrdsb.ca>
2017-12-14 13:16:39 -0500

    done A7q1

---
+++ src/Assigment7/A7Q4.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assigment7;
+
+/**
+ *
+ * @author gavra1029
+ */
+public class A7Q4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
---
+++ src/Assigment7/A7Q5.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assigment7;
+
+/**
+ *
+ * @author gavra1029
+ */
+public class A7Q5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
