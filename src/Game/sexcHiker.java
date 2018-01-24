0ab01cabf481f01f3ee458e016c9451306c7feda
Alexa <Alexa@192.168.0.25>
2018-01-23 22:37:13 -0500

    Fly boy fly

--- src/Game/sexcHiker.java
+++ src/Game/sexcHiker.java
@@ -9,6 +9,7 @@ import java.awt.Graphics;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Rectangle;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
@@ -36,7 +37,7 @@ public class sexcHiker extends JComponent {
     long desiredTime = (1000) / desiredFPS;
     // YOUR GAME VARIABLES WOULD GO HERE
     //player var
-    Rectangle player = new Rectangle(HEIGHT/2, WIDTH/2, 40, 80);
+    Rectangle player = new Rectangle(HEIGHT / 2, WIDTH / 2, 40, 80);
     //make a var the reps my angle
     double theta = 0;
     //where my mouse is
@@ -44,7 +45,7 @@ public class sexcHiker extends JComponent {
     //where the center of the player will be
     int cx, cy;
     //center of hammer
-    int cHX,cHY;
+    int cHX, cHY;
     //general x and y for the hammer
     int distanceOfMouseX, distanceOfMouseY;
     //general x and y for the hammer but useing the fly
@@ -54,17 +55,17 @@ public class sexcHiker extends JComponent {
     //setting what the distance will be 
     double distance = 0;
     //make a hammer 
-    Rectangle hammer = new Rectangle(mx, my, 20, 20);
+    Rectangle fly = new Rectangle(mx, my, 20, 20);
     //displacement in the x and y direction for player
     int playerDX = 0;
     int playerDY = 0;
     //the amount that the fly is being is being displaced
-    
+
     //displacement in the x and y direction for the hammer
     int hammerDX = 0;
     int hammerDY = 0;
     //gavity strenght
-    int gravity= 1;
+    int gravity = 1;
     //controll for player
     boolean left = false;
     boolean right = false;
@@ -72,42 +73,41 @@ public class sexcHiker extends JComponent {
     boolean onGround = false;
     //make a arry of rectangles
     Rectangle[] blocks = new Rectangle[1];
-    // making a var that dosent allow the hammer to pass through
-    boolean hammerCode= false;
+    
     //make the distance of the displacement to be a var
-    int distanceD=player.x;
+    int distanceD = player.x;
     //add colour to the game using image
-   
+
     BufferedImage pond = loadImage("pond.png");
     BufferedImage frog = loadImage("frog.png");
-    BufferedImage underwater =loadImage("underwater.jpg");
-    BufferedImage fly =loadImage("fly.png");
-    BufferedImage log=loadImage("log.png");
+    BufferedImage underwater = loadImage("underwater.png");
+    BufferedImage flyboyfly = loadImage("fly.png");
+    BufferedImage log = loadImage("log.png");
     //ball
     int ballSize = 60;
-    Rectangle ball = new Rectangle(600, 420,ballSize,ballSize);
-    int ballXDirection= -1;
-    int ballYDirection=0;
-    int ballSpeed=4;
+    Rectangle ball = new Rectangle(600, 420, ballSize, ballSize);
+    int ballXDirection = -1;
+    int ballYDirection = 0;
+    int ballSpeed = 4;
     //making ball go faster
-    double ballFaster= 0.2;
-    
+    double ballFaster = 0.2;
+    //game over 
+    int gameover;
+    //font
+    Font biggerFont = new Font("arial", Font.BOLD, 42);
+    Font bigistFont = new Font("arial", Font.BOLD, 70);
 
     // GAME VARIABLES END HERE   
-    
-    
-    public BufferedImage loadImage(String name){
+    public BufferedImage loadImage(String name) {
         BufferedImage img = null;
-        try{
+        try {
             img = ImageIO.read(new File(name));
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return img;
     }
-    
-    
-    
+
     // Constructor to create the Frame and place the panel in
     // You will learn more about this in Grade 12 :)
     public sexcHiker() {
@@ -146,32 +146,35 @@ public class sexcHiker extends JComponent {
         // GAME DRAWING GOES HERE
         //coulor for player
         //background
-        g.drawImage(pond, 0, 0, WIDTH, HEIGHT-100, null);
-        
-        
+        g.drawImage(pond, 0, 0, WIDTH, HEIGHT - 100, null);
+
         g.setColor(Color.BLUE);
         //draw the frog
-        g.drawImage(frog,player.x, player.y, player.width, player.height,null);
+        g.drawImage(frog, player.x, player.y, player.width, player.height, null);
         //colour for hammer
-        g.setColor(Color.DARK_GRAY);
         
-        //draw flay
-        g.drawImage(log,ball.x, ball.y, 30, 30,null);
 
+        //draw log
+        g.drawImage(log, ball.x, ball.y, 30, 30, null);
 
-        g.drawImage(fly,hammer.x, hammer.y, 30, 30,null);
+        g.drawImage(flyboyfly, fly.x, fly.y, 30, 30, null);
 
         g.setColor(Color.BLACK);
         for (int i = 0; i < blocks.length; i++) {
             g.fillRect(blocks[i].x, blocks[i].y, blocks[i].width, blocks[i].height);
         }
-        g.drawImage(underwater,blocks[0].x, blocks[0].y, blocks[0].width, blocks[0].height,null);
+        g.drawImage(underwater, blocks[0].x, blocks[0].y, blocks[0].width, blocks[0].height, null);
         g.fillRect(ball.x, ball.y, ball.width, ball.height);
+        // draw gameover
+        g.setFont(biggerFont);
+        g.drawString("Leap Frocka", WIDTH / 2 - 100, 50);
+        g.setFont(bigistFont);
+        if (player.intersects(ball)) {
+            g.drawString("GAME OVER", WIDTH / 2 - 200, 150);
+        }
 
         //g.setColor(Color.RED);
         //g.fillRect(player.x + player.width / 2, player.y + player.height / 2, 150, 1);
-
-
         // GAME DRAWING ENDS HERE
     }
 
@@ -181,11 +184,7 @@ public class sexcHiker extends JComponent {
         // Any of your pre setup before the loop starts should go here
         //if the width of the log is greater than the width rest the log to 600( the other side of the sreen
 
-        blocks[0] = new Rectangle(0, 480, WIDTH, 125 );
-
-
-
-
+        blocks[0] = new Rectangle(0, 480, WIDTH, 125);
 
     }
 
@@ -208,37 +207,34 @@ public class sexcHiker extends JComponent {
 
             // all your game rules and move is done in here
             // GAME LOGIC STARTS HERE 
-            ball.x=ball.x+ballXDirection*ballSpeed;
+            ball.x = ball.x + ballXDirection * ballSpeed;
             //make ball go back
-            if (ball.x<0 ) {
-                ball.x=WIDTH;
+            if (ball.x < 0) {
+                ball.x = WIDTH;
             }
             //make ball go faster
-            ballSpeed=ballSpeed+(int)(ballFaster);
-            
-            
+            ballSpeed = ballSpeed + (int) (ballFaster);
 
             // making the hammer rotate rounf the play useing trig
-
             //setting cx and cy to the center of the plaayer
             cx = player.x + player.width / 2;
             cy = player.y + player.height / 2;
             //calulate the center of the hammer for x and y
-            cHX=hammer.y + hammer.width / 2;
-            cHY=hammer.y + hammer.width /2;
-            
+            cHX = fly.y + fly.width / 2;
+            cHY = fly.y + fly.width / 2;
+
             //cal the x and y postion for the hammer
             distanceOfMouseX = mx - cx;
             distanceOfMouseY = my - cy;
             //calulate the x and y postion for the hammer but use the center of the hammer not the player
             distanceOfMouseFX = mx - cHX;
             distanceOfMouseFY = my - cHY;
-            
+
             // start the the movement system for the player
             if (right) {
-                playerDX = 3;
+                playerDX = 5;
             } else if (left) {
-                playerDX = -3;
+                playerDX = -5;
 
             } else {
                 playerDX = 0;
@@ -247,14 +243,23 @@ public class sexcHiker extends JComponent {
             if (onGround && jump) {
                 //do someting ab jumping
                 //big neg num to start move 
-                playerDY = -10;
+                playerDY = -25;
                 onGround = false;
             }
             //add gavity
-           playerDY= playerDY + gravity;
+            playerDY = playerDY + gravity;
+            //colition for the player if the frog hits the ball
+            if (player.intersects(ball)) {
+                ballSpeed = 0;
+                if (right) {
+                    playerDX = 0;
+                } else if (left) {
+                    playerDX = 0;
 
+                }
+                playerDY=0;
 
-
+            }
 
             //update player
             player.x = player.x + playerDX;
@@ -262,8 +267,6 @@ public class sexcHiker extends JComponent {
 
             //assume that user is falling
             //onGround = false;
-
-
             //check for collition
             //go through each block
             for (int i = 0; i < blocks.length; i++) {
@@ -294,7 +297,6 @@ public class sexcHiker extends JComponent {
                                 onGround = true;
                             }
 
-
                         } else {
                             player.y = player.y + cHeight;
 
@@ -311,18 +313,11 @@ public class sexcHiker extends JComponent {
             //use soh cah toa to find the distance of hammer x and hammer y
             //calulate hammer x
             hammerXF = Math.cos(thetaF) * 150;
-                //calulate hammer Y
+            //calulate hammer Y
             hammerYF = 150 * Math.sin(thetaF);
-
-
-
-
-
 
             //callulate theta
             double theta = Math.atan2(distanceOfMouseY, distanceOfMouseX);
-
-
 
             //calulate 
             double hammerRX = 150 * Math.cos(theta);
@@ -345,31 +340,27 @@ public class sexcHiker extends JComponent {
             }
             //refresh hammer
 
-            hammer.x = cx + (int) (hammerX);
-            hammer.y = cy + (int) (hammerY);
+            fly.x = cx + (int) (hammerX);
+            fly.y = cy + (int) (hammerY);
 
             //calulating the collition system for the hammer
             for (int i = 0; i < blocks.length; i++) {
-                if (hammer.intersects(blocks[i])) {
-                    int cHeight = Math.min(blocks[i].y + blocks[i].height, hammer.y + hammer.height) - Math.max(blocks[i].y, hammer.y);
-                    int cWidth = Math.min(blocks[i].x + blocks[i].width, hammer.x + hammer.width) - Math.max(blocks[i].x, hammer.x);
+                if (fly.intersects(blocks[i])) {
+                    int cHeight = Math.min(blocks[i].y + blocks[i].height, fly.y + fly.height) - Math.max(blocks[i].y, fly.y);
+                    int cWidth = Math.min(blocks[i].x + blocks[i].width, fly.x + fly.width) - Math.max(blocks[i].x, fly.x);
                     //making the block not go through the floor
-                    
-                    
-                    
-                    System.out.println("h: " + cHeight +  "   w: " + cWidth);
-                    
-                    
+
+                    System.out.println("h: " + cHeight + "   w: " + cWidth);
+
                     //see what is smaller
                     if (cWidth < cHeight) {
                         //fix player
                         //player on left sode
-                        if (hammer.x < blocks[i].x) {
-                            hammer.x = hammer.x - cWidth;
-
+                        if (fly.x < blocks[i].x) {
+                            fly.x = fly.x - cWidth;
 
                         } else {
-                            hammer.x = hammer.x + cWidth;
+                            fly.x = fly.x + cWidth;
 
                         }
                         //stop player to move side whys
@@ -377,8 +368,8 @@ public class sexcHiker extends JComponent {
 
                     } else {
                         //above the block
-                        if (hammer.y < blocks[i].y) {
-                            hammer.y = hammer.y - cHeight;
+                        if (fly.y < blocks[i].y) {
+                            fly.y = fly.y - cHeight;
 
                             //moving up or down?
                             if (hammerDY >= 0) {
@@ -389,26 +380,13 @@ public class sexcHiker extends JComponent {
                                 onGround = true;
                             }
 
-
                         } else {
-                            hammer.y = hammer.y - (hammer.y-blocks[i].y) - 30;
-
+                            fly.y = fly.y - (fly.y - blocks[i].y) - 30;
 
                         }
                     }
                 }
             }
-                    
-           
-
-
-
-
-
-
-
-
-
 
             // GAME LOGIC ENDS HERE 
             // update the drawing (calls paintComponent)
